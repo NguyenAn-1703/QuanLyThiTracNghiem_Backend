b4826d2986270d8e9316057ee93b45d1a8f65c7e
Hiennguyen278610 <thanhhien00000005@gmail.com>
2026-01-30 10:24:59 +0700

    feat: add Handler exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,24 +1,37 @@
 <?php
-
 namespace App\Exceptions;
 
+use App\Traits\ApiResponseTrait;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponseTrait;
+
     /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<Throwable>>
+     * Chặn log các exception thông dụng
+     * @var array
      */
     protected $dontReport = [
-        //
+        ValidationException::class,
+        AuthenticationException::class,
+        AuthorizationException::class,
+        ModelNotFoundException::class,
+        NotFoundHttpException::class,
+        ThrottleRequestsException::class,
     ];
 
     /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
+     * Chặn cơ chế auto lưu các field quan trọng của laravel để bảo mật
      * @var array<int, string>
      */
     protected $dontFlash = [
@@ -28,14 +41,56 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * Register the exception handling callbacks for the application.
-     *
+     * Hàm điều hướng exception
+     * $this->reportable(): điều hướng các log hệ thống
+     * $this->renderable(): hiển thị cho client
      * @return void
      */
-    public function register()
+    public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->reportable(function (QueryException $e) {
+            Log::error("DATABASE ERROR: " . $e->getMessage(), [
+                'sql'      => $e->getSql(),
+                'bindings' => $e->getBindings(),
+            ]);
         });
+
+        $this->renderable(function (Throwable $e, $request) {
+            if ($request->is('api/*') || $request->expectsJson()) {
+                return $this->handleApiException($e);
+            }
+        });
+    }
+
+    /**
+     * Method tiện ích cho register
+     * @param Throwable $e
+     * @return \Illuminate\Http\JsonResponse
+     */
+    private function handleApiException(Throwable $e)
+    {
+        $msg = $e->getMessage();
+        return match (true) {
+            $e instanceof ValidationException       => $this->validation($e->errors(), 'Dữ liệu không hợp lệ'),
+
+            $e instanceof AuthenticationException   => $this->unauthorized(),
+
+            $e instanceof AuthorizationException    => $this->forbidden(),
+
+            $e instanceof ThrottleRequestsException => $this->throttleRequest(),
+
+            $e instanceof ModelNotFoundException    => $this->notFound(
+                class_basename($e->getModel() . "Không tìm thấy")
+            ),
+
+            $e instanceof HttpException             => $this->error(null, $msg, $e->getCode()),
+
+            $e instanceof QueryException            => $this->error(
+                config('app.debug') ? "Lỗi database: " . $msg : null
+            ),
+
+            default                                 => $this->serverError(
+                config('app.debug') ? $msg : null)
+        };
     }
 }
