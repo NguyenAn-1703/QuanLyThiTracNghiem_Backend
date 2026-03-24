bd5dbaa49bfc1eb21a324e108869483c540e018f
Hiennguyen278610 <thanhhien00000005@gmail.com>
2026-03-25 06:47:57 +0700

    chore(cauhinhthi-api): change blind code

--- routes/api/cauhinhthi.php
+++ routes/api/cauhinhthi.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\CauHinhThiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/cauhinhthis', [CauHinhThiController::class, 'index']);
-Route::get('/cauhinhthis/{cauHinhThi}', [CauHinhThiController::class, 'show']);
+Route::get('/cauhinhthis/{cauhinhthi}', [CauHinhThiController::class, 'show']);
 Route::post('/cauhinhthis', [CauHinhThiController::class, 'store']);
-Route::put('/cauhinhthis/{cauHinhThi}', [CauHinhThiController::class, 'update']);
-Route::delete('/cauhinhthis/{cauHinhThi}', [CauHinhThiController::class, 'destroy']);
+Route::put('/cauhinhthis/{cauhinhthi}', [CauHinhThiController::class, 'update']);
+Route::delete('/cauhinhthis/{cauhinhthi}', [CauHinhThiController::class, 'destroy']);
