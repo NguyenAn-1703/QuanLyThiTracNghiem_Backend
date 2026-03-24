be21bc61bfe853fa69e12e2e9f8edc7e883eccef
Hiennguyen278610 <thanhhien00000005@gmail.com>
2026-03-24 23:45:38 +0700

    feat(CauHoi-Model): add seed  and fix seed  pivot table

--- app/Models/CauHoi.php
+++ app/Models/CauHoi.php
@@ -9,8 +9,11 @@ class CauHoi extends Model
 {
     use HasFactory;
 
+    protected $table = 'cau_hois';
+
     protected $fillable = [
         "monHocId",
+        "chuongId",
         "noiDungCauHoi",
         "doKhoId",
         "imageUrl",
@@ -27,13 +30,37 @@ class CauHoi extends Model
     protected $casts = [
         "diemMacDinh" => "decimal:2",
         "isDeleted" => "boolean",
+        "created_at" => "datetime",
+        "updated_at" => "datetime",
     ];
 
-    public function deThis(){
-        return $this->belongsToMany(DeThi::class, 'chiTietDeThi', 'cauHoiId', 'deThiId');
+    public function monHoc()
+    {
+        return $this->belongsTo(MonHoc::class, 'monHocId');
     }
 
-    public function cauTraLois(){
+    public function chuong()
+    {
+        return $this->belongsTo(Chuong::class, 'chuongId');
+    }
+
+    public function doKho()
+    {
+        return $this->belongsTo(DoKho::class, 'doKhoId');
+    }
+
+    public function nguoiTao()
+    {
+        return $this->belongsTo(User::class, 'nguoiTaoId');
+    }
+
+    public function deThis()
+    {
+        return $this->belongsToMany(DeThi::class, 'chi_tiet_de_this', 'cauHoiId', 'deThiId');
+    }
+
+    public function cauTraLois()
+    {
         return $this->hasMany(CauTraLoi::class, 'cauHoiId');
     }
 }
