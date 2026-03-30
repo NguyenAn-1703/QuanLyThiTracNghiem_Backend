08f2d6abac1c66da1da8b914f49cd6c4449bf753
Hiennguyen278610 <thanhhien00000005@gmail.com>
2026-03-31 00:13:21 +0700

    feat: add API routes for NhomHocPhans import and export

--- routes/api/nhomhocphan.php
+++ routes/api/nhomhocphan.php
@@ -11,6 +11,8 @@ Route::put('/nhomhocphans/{nhomhocphan}', [NhomHocPhanController::class, 'update
 Route::delete('/nhomhocphans/{nhomhocphan}', [NhomHocPhanController::class, 'destroy']);
 Route::get('/nhomhocphans/o_svien/{user}', [NhomHocPhanController::class, 'get_o_svien']);
 Route::get('/nhomhocphans/get_danh_sach_sinh_vien/{nhomhocphan}', [NhomHocPhanController::class, 'get_danh_sach_sinh_vien']);
+Route::get('/nhomhocphans/sinh_vien_export/{nhomhocphan}', [NhomHocPhanController::class, 'sinhVienExport']);
+Route::post('/nhomhocphans/sinh_vien_import/{nhomhocphan}', [NhomHocPhanController::class, 'sinhVienImport']);
 Route::post('/nhomhocphans/add_sinh_vien_to_nhom/{nhomhocphan}', [NhomHocPhanController::class, 'add_sinh_vien_to_nhom']);
 Route::get('/nhomhocphans/w_gvien_mon/{nhomhocphan}', [NhomHocPhanController::class, 'get_w_gvien_mon']);
 Route::get('/nhomhocphans/w_dekiemtra/{nhomhocphan}', [NhomHocPhanController::class, 'get_w_dekiemtra']);
